07bce3755b6fb56b7760c1e28e7bbf416e976f9f
Pedro Oleirinha <oleirinha.pedro@gmail.com>
2025-07-24 12:36:13 +0100

    finished the factories and seeders for each model. The api GET request is working properly to show the event info with attendees

---
+++ app/Http/Controllers/Api/EventController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Event::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'required|date',
+            'end_time' => 'required|date|after:start_time'
+        ]);
+        $event = Event::create(
+            [
+                ...$data,
+                'user_id' => 1
+            ]
+        );
+        return $event->toJson();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Event $event)
+    {
+        //
+        return $event->load("attendees");
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- database/migrations/2025_07_23_182146_create_events_table.php
+++ database/migrations/2025_07_23_182146_create_events_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(User::class);
-            $table->string('description')->nullable();
+            $table->string('name');
+            $table->text('description')->nullable();
 
             $table->dateTime('start_time');
             $table->dateTime('end_time');
